11780966d395679e05ee2302c040b11a156ffbc7
Laurie Stephey <lastephey@lbl.gov>
2020-04-21 21:46:07 -0700

    progress

--- benchmark_orchestrator.py
+++ benchmark_orchestrator.py
@@ -2,6 +2,9 @@
 
 import argparse
 import os
+from gpu_study.pycuda import timeit_pycuda_legval
+from gpu_study.numba import timeit_numba_legval
+from gpu_study.cupy import timeit_cupy_legval
 
 #this script should orchestrate all the benchmarks as a part of our python on gpus study
 
@@ -12,37 +15,21 @@ import os
 #kernelsize
 #where to write what to name output? or have that happen automatically?
 
-
-#reads argparse
-
-#outer loop: which benchmark
-
-#next loop: which framework
-
-#next loop: arraysize
-
-#innermost loop: blocksize
-
 def parse_arguments():
     print("parsing data")
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--tests', '-t', type=str, default='all',
-                        help='which kernels to run')
-    parser.add_argument('--frameworks', '-f', type=str, default='all',
-                        help='sum the integers (default: find the max)')
+    parser.add_argument('--frameworks', '-f', type=str, default=['cupy','numba','pycuda'],
+                        help='which frameworks to use')
+    parser.add_argument('--benchmarks', '-b', type=str, default=['legval'],
+                        help='which benchmarks to run')
     parser.add_argument('--arraysize', '-a', type=int
                         help='size of array for benchmarks')
-    parser.add_argument('--blocksize', '-b', type=int,
+    parser.add_argument('--blocksize', '-s', type=int, default=32,
                         help='blocksize for gpu kernels')
-    parser.add_argument('--ntests', '-n', type=int, default=10,
+    parser.add_argument('--ntests', '-n', type=int, default=100,
                         help='how many times timeit will run each test')
     args = parser.parse_args()
     return args
-
-def launch_kernel():
-
-    return
-
 
 def write_output():
 
@@ -51,17 +38,20 @@ def write_output():
 
 def main():
     args = parse_arguments()
-    print("tests", args.tests)
     print("frameworks", args.frameworks)
-    print("arraysize", args.arraysize) #all unless user specifies otherwise
-    print("blocksize", args.blocksize) #all unless user specifies otherwise
+    print("benchmarks", args.frameworks)
+    print("arraysize", args.arraysize)
+    print("blocksize", args.blocksize)
     print("ntests", args.ntests)
     
-    #outermost loop over tests
-    for test in tests:
+    #outermost loop over frameworks
+    for framework in frameworks:
     
-        #loop over frameworks
-        for framework in frameworks:
+        #loop over benchmarks
+        for benchmark in benchmarks:
+
+            #generate function names
+            test_name="timeit_" + framework + "_" + benchmark
 
             #loop over arraysizes
             for a in arraysize:
@@ -69,9 +59,8 @@ def main():
                 #blocksize may or may not be a good parameter to loop over
                 #some kernels really sensitive to block size, will crash with bad choice
                 #loop over gpu kernel blocksize (depending on framework)
-                if framework = pycuda 
                 for b in blocksize:
-
+                    test_string="timeit_ {} _ {} .format(framework, benchmark, arraysize, )
 
     write_output()
  
