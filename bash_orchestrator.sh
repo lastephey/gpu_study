be728630c06a7cb0c59de3f76973a528166604ef
Laurie Stephey <lastephey@lbl.gov>
2020-05-27 13:03:37 -0700

    loop over arraysize

--- bash_orchestrator.sh
+++ bash_orchestrator.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#needs to be bash so we can hop in and out of conda envs
+#bash lets us hop in and out of conda envs
 
 #write logfile
 LOG_FILE=/global/cscratch1/sd/stephey/gpu_study/results/log_${SLURM_JOB_ID}.out
@@ -11,19 +11,22 @@ exec 3>&1 1>${LOG_FILE} 2>&1
 module load python
 
 #settings
-array_size=10000
 benchmark=eigh
 declare -a FRAMEWORKS=("numpy" "cupy" "jax")
+declare -a ARRAYSIZES=("100" "200" "500" "1000" "2000" "5000")
 
 #loop over requested frameworks
 for f in "${FRAMEWORKS[@]}"
-do
-    source activate $f
-    printf "$f"
-    srun -n 1 python run_benchmark.py -f $f -a $array_size -b $benchmark
-    source deactivate
-done
-
+    #loop over requested arraysizes
+    do    
+    for s in "${ARRAYSIZES[@]}"
+        do
+            source activate $f
+            printf "$f"
+            srun -n 1 python run_benchmark.py -f $f -a $s -b $benchmark
+            source deactivate
+        done
+    done
 echo
 
 
