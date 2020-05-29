255ffb167b7145e68cee985e1d1c8c371486148c
Laurie Stephey <lastephey@lbl.gov>
2020-05-28 17:54:41 -0700

    progress

--- bash_orchestrator.sh
+++ bash_orchestrator.sh
@@ -3,18 +3,19 @@
 #bash lets us hop in and out of conda envs
 
 #write logfile
-LOG_FILE=/global/cscratch1/sd/stephey/gpu_study/results/log_${SLURM_JOB_ID}.out
-echo $LOG_FILE
-exec 3>&1 1>${LOG_FILE} 2>&1
+#LOG_FILE=/global/cscratch1/sd/stephey/gpu_study/results/log_${SLURM_JOB_ID}.out
+#echo $LOG_FILE
+#exec 3>&1 1>${LOG_FILE} 2>&1
 
 #source your favorite python
 module load python
 
 #array settings
-declare -a BENCHMARKS=("legval" "eigh")
-declare -a FRAMEWORKS=("numpy" "cupy" "jax")
-declare -a ARRAYSIZES=("100" "200" "500" "1000" "2000" "5000")
-
+declare -a BENCHMARKS=("legval")
+declare -a FRAMEWORKS=("numpy")
+declare -a ARRAYSIZES=("100")
+declare -a BLOCKSIZE=("32")
+declare -a PRECISION=("float64")
 
 #loop over requested frameworks
 for f in "${FRAMEWORKS[@]}"
@@ -25,11 +26,19 @@ for f in "${FRAMEWORKS[@]}"
         #loop over requested arraysizes   
         for s in "${ARRAYSIZES[@]}"
             do
-                source activate $f
-                printf "$f"
-                srun -n 1 python run_benchmark.py -f $f -a $s -b $benchmark
-                source deactivate
-            done
+	    #loop over requested blocksize (will only apply to some frameworks)
+            for k in "${BLOCKSIZE[@]}"
+                do
+                #loop over requested precision
+	        for p in "${PRECISION[@]}"
+	            do    
+                        source activate $f
+                        printf "framework: $f benchmark: $b arraysize: $s blocksize: $k precision: $p"
+                        srun -n 1 python run_benchmark.py -f $f -b $b -a $s -k $k -p $p
+                        source deactivate
+	            done    
+                done
+            done		
         done
     done
 echo
--- run_benchmark.py
+++ run_benchmark.py
@@ -7,13 +7,10 @@ import datetime
 
 import numpy as np
 
-#TODO: switch to OOP
-#TODO: make sure we can handle single and double precision
 #TODO: find some way to time the data movement separately (or remove it) via separate timeit calls
 #TODO: generate the data once and re-use it?
 #TODO: other benchmarks
 #TODO: add ability to skip benchmarks that are not implemented or precision that is not possible
-#TODO: add more layers to bash orchestrator
 
 class BenchTask:
 
@@ -25,56 +22,39 @@ class BenchTask:
         self.arraysize = args.arraysize
         self.blocksize = args.blocksize
         self.precision = args.precision
-        self.ntrials = args.number #these might be backwards
-        self.nruns = args.repeat 
-
-    def create_benchmark_data(self):
-        """
-        This function will create the appropriate data
-        for the requested benchmark, taking into account
-        array dimension (1,2,3D?) and precision
-
-        Input: benchmark, precision
-
-        Ouput: input_data
-        """
+        self.ntrials = args.ntrials
+        self.ntests = args.ntests
 
         #continue adding as we incorporate additional benchmarks
-        if self.bencharmark == 'legval':
+        if self.benchmark == 'legval':
             #legval input is 1D
-            self.input_data = np.random.rand(self.arraysize).astype(precision)
+            self.input_data = np.random.rand(self.arraysize).astype(self.precision)
 
         elif self.benchmark == 'eigh':
             #eigh input is 2D
-            self.input_data = np.random.rand((self.arraysize, self.arraysize)).astype(precision)
+            self.input_data = np.random.rand((self.arraysize, self.arraysize)).astype(self.precision)
 
         else:
             print("No information for requested benchamark")
             exit()
 
-        return    
-
-    def create_filenames(self):
-        """
-        point to where the reference data for each BenchTask will live
-        """
-
         #hardcode path for now
         location = '/global/cscratch1/sd/stephey/gpu_study/results/'
 
-        #for where we save the data (used in correctness checking)
-        self.data_filename = location + str(self.framework) + '_' + str(self.benchmark) +
-                        '_' + str(self.arraysize) + '_' + str(self.blocksize) + '_' + 
-                        (self.precision)'.npy'
+        #establish filename for output data (used in correctness checking)
+        self.data_filename = (location + str(self.framework) + '_' + str(self.benchmark)
+                             + '_' + str(self.arraysize) + '_' + str(self.blocksize) + '_'
+                             + (self.precision) + '.npy')
 
-        #for the reference numpy data file
-        self.ref_filename = location + 'numpy_' + str(benchmark) + '_' +
-                     str(arraysize) + '_' + str(blocksize) + '.npy'
+        #establish filename for the reference numpy data file
+        self.ref_filename = (location + 'numpy_' + str(self.benchmark) + '_' +
+                            str(self.arraysize) + '_' + str(self.blocksize) + '_' + 
+                            str(self.precision) + '.npy')
 
-        #for the timeit file
-        self.timeit_filename = timeit_filename = location + 
-                               'timeit_{}_{}_{}_{}'.format(self.framework, self.benchmark, 
-                                self.arraysize, now)
+        #establish filename for the timeit file
+        now = datetime.datetime.now()
+        self.timeit_filename = (location + 'timeit_{}_{}_{}_{}_{}_{}'.format(self.framework, 
+                               self.benchmark, self.arraysize, self.blocksize, self.precision, now))
         return                       
 
 
@@ -92,27 +72,24 @@ class BenchTask:
     
         #TODO: figure out how to reuse our same data x_input
     
-        timeit_setup = 'import {}_framework; arraysize={}; blocksize={}'\
-                       .format(framework, arraysize, blocksize)
+        timeit_setup = 'import {}_framework; arraysize={}; blocksize={}; precision={}'\
+                       .format(self.framework, self.arraysize, self.blocksize, self.precision)
         #print(timeit_setup)               
-        timeit_code = 'results = {}_framework.{}_{}(arraysize, blocksize)'\
-                      .format(framework, framework, benchmark)
+        timeit_code = 'results = {}_framework.{}_{}(arraysize, blocksize, precision)'\
+                      .format(self.framework, self.framework, self.benchmark)
         #print(timeit_code)              
-        timeit_list = timeit.repeat(setup=timeit_setup, stmt=timeit_code, repeat=repeat, number=number)
+        timeit_list = timeit.repeat(setup=timeit_setup, stmt=timeit_code, repeat=self.ntrials, number=self.ntests)
         print('Min {} {} time of {} trials, {} runs each: {}'\
-              .format(framework, benchmark, repeat, number, min(timeit_list)))
+              .format(self.framework, self.benchmark, self.ntrials, self.ntests, min(timeit_list)))
     
         #can we run timeit again with the same setup? yes, here is how:
-        timeit_test = timeit.repeat(setup=timeit_setup, stmt="import numpy as np", repeat=repeat, number=number)
+        timeit_test = timeit.repeat(setup=timeit_setup, stmt="import numpy as np", repeat=self.ntrials, number=self.ntests)
         print("numpy imported as test")
     
         timeit_data = np.array(timeit_list)
     
         #need to save timeit data in addition to printing them
-        now = datetime.datetime.now()
-        location = '/global/cscratch1/sd/stephey/gpu_study/results/'
-        timeit_filename = location + 'timeit_{}_{}_{}_{}'.format(framework, benchmark, arraysize, now)
-        np.save(timeit_filename, timeit_data)
+        np.save(self.timeit_filename, timeit_data)
     
         return timeit_data                               
 
@@ -132,12 +109,8 @@ class BenchTask:
         submodule = str(framework) + '_' + str(benchmark)
         results = getattr(module, submodule)(arraysize, blocksize)
     
-        location = '/global/cscratch1/sd/stephey/gpu_study/results/'
     
-        filename = location + str(framework) + '_' + str(benchmark) + '_' +
-                   str(arraysize) + '_' + str(blocksize) + '.npy'
-    
-        np.save(filename, results)
+        np.save(self.data_filename, results)
         return
 
     def correctness_check(self):
@@ -156,13 +129,13 @@ class BenchTask:
     
         location = '/global/cscratch1/sd/stephey/gpu_study/results/'
     
-        filename = location + str(framework) + '_' + str(benchmark) + 
-                   '_' + str(arraysize) + '_' + str(blocksize) + '.npy'
+        filename = (location + str(framework) + '_' + str(benchmark) + 
+                   '_' + str(arraysize) + '_' + str(blocksize) + '.npy')
     
         results = np.load(filename,allow_pickle=True)
     
-        numpy_filename = location + 'numpy_' + str(benchmark) + '_' + 
-                         str(arraysize) + '_' + str(blocksize) + '.npy'
+        numpy_filename = (location + 'numpy_' + str(benchmark) + '_' + 
+                         str(arraysize) + '_' + str(blocksize) + '.npy')
     
         #load numpy results to compare via np.allclose               
         numpy_results = np.load(numpy_filename,allow_pickle=True)
@@ -184,13 +157,13 @@ def parse_arguments():
                         help='which framework to use')
     parser.add_argument('--benchmark', '-b', default=['legval'],
                         help='which benchmark to run')
-    parser.add_argument('--array', '-a', type=int,  default=100,
+    parser.add_argument('--arraysize', '-a', type=int,  default=100,
                         help='size of array for benchmarks')
-    parser.add_argument('--blocksize', '-s', type=int, default=32,
+    parser.add_argument('--blocksize', '-k', type=int, default=32,
                         help='blocksize for gpu kernels')
-    parser.add_argument('--precision', '-p', type=str, default='double',
-                        help='run benchmarks in single or double precision')
-    parser.add_argument('--repeat', '-r', type=int, default=3,
+    parser.add_argument('--precision', '-p', type=str, default='float64',
+                        help='run benchmarks using either float32 or float64')
+    parser.add_argument('--ntrials', '-t', type=int, default=3,
                         help='how many times timeit will run')
     parser.add_argument('--ntests', '-n', type=int, default=10,
                         help='how many times timeit will run each test')
@@ -213,29 +186,31 @@ def main():
     #create BenchTask object 
     benchtask = BenchTask(args)
 
-    print(benchtask)
-
-    #call our function to generate the right kind of data
-    benchtask.create_benchmark_data()
+    print("benchtask.framework", benchtask.framework)
+    print("benchtask.benchmark", benchtask.benchmark)
+    print("benchtask.input_data.shape", benchtask.input_data.shape)
 
     #now run the benchtask
-    results = time_kernel(self)
+    timeit_data = benchtask.time_kernel()
 
-    #if framework = numpy. need to save the files and get them ready
-    #for correctness checking
-    get_save_results(framework, benchmark, arraysize, blocksize)
+    print("timeit_data:", timeit_data)
 
-    #now see if those results are actually correct (agree with numpy, anyway)
-    if framework != 'numpy':
-        correct = correctness_check(framework, benchmark, arraysize, blocksize, precision)
-    else:
-        correct = True #for numpy
-    if correct == False:
-        print('{} {} {} results do not agree with numpy'.format(framework, benchmark, precision))
-    else:
-        print('results agree')
-
-    return
+###
+###    #if framework = numpy. need to save the files and get them ready
+###    #for correctness checking
+###    get_save_results(framework, benchmark, arraysize, blocksize)
+###
+###    #now see if those results are actually correct (agree with numpy, anyway)
+###    if framework != 'numpy':
+###        correct = correctness_check(framework, benchmark, arraysize, blocksize, precision)
+###    else:
+###        correct = True #for numpy
+###    if correct == False:
+###        print('{} {} {} results do not agree with numpy'.format(framework, benchmark, precision))
+###    else:
+###        print('results agree')
+###
+###    return
 
 if __name__ == "__main__":
     main()
